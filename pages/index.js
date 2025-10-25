df9e6d03576f318ca56be92ace54d1620665477b
ytouchton-tech <ytouchton@gmail.com>
2025-10-25 02:48:56 -0700

    Update and rename index.html to pages/index.js
    Signed-off-by: ytouchton-tech <ytouchton@gmail.com>

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontFamily: "system-ui, -apple-system, Segoe UI, Roboto, Helvetica, Arial"
+    }}>
+      <div style={{textAlign: "center", padding: 24}}>
+        <h1 style={{fontSize: 32, margin: 0}}>MIND VOYAGE SUPPLY CO.</h1>
+        <p style={{fontSize: 18, marginTop: 8}}>GET INOCK’D UP</p>
+      </div>
+    </main>
+  );
+}
